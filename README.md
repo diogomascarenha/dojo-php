195d9136ff989a78a0bb059750cf3fb349cd440c
Diogo Oliveira Mascarenhas <diogomascarenha@gmail.com>
2016-10-19 15:20:46 -0200

    Exemplo de Testes com PHPUnit e Docker

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dojo-php
 Exemplo de PHPUnit para o Dojo da Finxi
+
+```
+docker run -v $(pwd):/app --rm phpunit/phpunit run Tests/DojoTest.php
+```
---
+++ Tests/DojoTest.php
@@ -0,0 +1,46 @@
+<?php
+
+class ClientTest extends \PHPUnit_Framework_TestCase
+{
+	public function testBooleanSample()
+	{
+		$bool = true;
+		$this->assertTrue($bool);
+
+		$bool = false;
+		$this->assertFalse($bool);
+	}
+
+	public function testEqualsSample()
+	{
+		$string = 'abc';
+		$this->assertEquals('abc',$string);
+	}
+
+	public function testInstanceOfSample()
+	{
+		$date = new \Datetime();
+		$this->assertInstanceOf('Datetime',$date);
+	}
+
+	public function testCountSample()
+	{
+		$array = array(1,2,3,4,5);
+		$this->assertCount(5,$array);
+
+	}
+
+	public function testNullSample()
+	{
+		$null = null;
+		$this->assertNull($null);
+	}
+
+	public function testArrayHasKey()
+	{
+		$array = array ('position1' => 1, 'position2' => 1);
+    $this->assertArrayHasKey('position2',$array);
+
+		$this->assertArrayNotHasKey('position3',$array);
+	}
+}
